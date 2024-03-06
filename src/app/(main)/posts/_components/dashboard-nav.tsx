b30a9a72a7c258dedd59d4b6bc84380ca5714658
Fr3akyMurk <mark@fr3akymurk.eu>
2024-03-06 13:53:46 +0100

    Update dashboard-nav.tsx
    Add the posts button to the navbar

--- src/app/(main)/posts/_components/dashboard-nav.tsx
+++ src/app/(main)/posts/_components/dashboard-nav.tsx
@@ -8,11 +8,10 @@ import { cn } from "@/lib/utils";
 
 const items = [
   {
-    title: "Posts",
+    title: "Dashboard",
     href: "/dashboard",
     icon: FileTextIcon,
   },
-
   {
     title: "Billing",
     href: "/dashboard/billing",
@@ -22,6 +21,11 @@ const items = [
     title: "Settings",
     href: "/dashboard/settings",
     icon: GearIcon,
+  },
+  {
+    title: "Posts",
+    href: "/posts",
+    icon: FileTextIcon,
   },
 ];
 
